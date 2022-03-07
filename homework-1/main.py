80b97206f2fa27deb34d08bd86989b86d10f38e3
hojjatre <hojjat7878.h@gmail.com>
2022-03-07 17:48:50 +0330

    remove-null

--- homework-1/main.py
+++ homework-1/main.py
@@ -10,4 +10,13 @@ def load_data(path):
 
     return df_selected
 
+def remove_null(data):
+    
+    df = data.dropna(axis=0, subset=['content'])
+    df.to_excel('final_books_without_null.xlsx')
+
+    return df
+
 df = load_data('final_books.xlsx')
+
+df = remove_null(df)
