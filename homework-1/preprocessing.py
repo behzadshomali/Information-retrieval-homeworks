ed9900e043be5b0c8fff64a7ee175d3fbfd3181b
Behzad Shomali <behzad.shomali@gmail.com>
2022-03-08 00:26:05 +0330

    Added normalization method Changed "stemmer.py" --> "preprocessing.py" so every preprocessing method can be implemented in this file

--- homework-1/preprocessing.py
+++ homework-1/preprocessing.py
@@ -1,8 +1,18 @@
 import hazm as hzm
 import pandas as pd
+
+
 def stemmer(data):
   stemmer = hzm.Stemmer()
   stem_list = [None] * len(data)
   for i in range(len(data)):
     stem_list[i] = stemmer.stem(data[i])
   return stem_list
+
+def normalizer(data):
+  '''
+  data would be a single 
+  row of dataframe
+  '''
+  normalizer = hzm.Normalizer()
+  return normalizer.normalize(data)
