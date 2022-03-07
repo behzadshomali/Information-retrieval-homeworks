c94a08d84027c737324f3f283533882ff470fb35
hojjatre <hojjat7878.h@gmail.com>
2022-03-08 02:15:37 +0330

    normal->stopWord->lemma

--- homework-1/main.py
+++ homework-1/main.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+from preprocessing import process_normal_sw_lemma
 
 def load_data(path):
     df = pd.read_excel(path)
@@ -11,12 +11,18 @@ def load_data(path):
     return df_selected
 
 def remove_null(data):
-    
+
     df = data.dropna(axis=0, subset=['content'])
     df.to_excel('final_books_without_null.xlsx')
 
     return df
 
-df = load_data('final_books.xlsx')
+# df = load_data('final_books.xlsx')
+# df = remove_null(df)
 
-df = remove_null(df)
+
+df = load_data('final_books_without_null.xlsx')
+df['stop_word'] = ''
+df['lemmatizer'] = ''
+
+process_normal_sw_lemma(df)
--- homework-1/preprocessing.py
+++ homework-1/preprocessing.py
@@ -16,3 +16,28 @@ def normalizer(data):
   '''
   normalizer = hzm.Normalizer()
   return normalizer.normalize(data)
+
+def removeStopWords(text_tokens):
+  tokens_without_sw = [word for word in text_tokens if not word in hzm.stopwords_list()]
+  return tokens_without_sw
+
+def lemma(text_tokens):
+  lemmatizer = hzm.Lemmatizer()
+  temp = []
+  for word in text_tokens:
+    temp.append(lemmatizer.lemmatize(word))
+  return temp
+
+def process_normal_sw_lemma(data):
+  for index in data.index:
+    normalized = normalizer(data['content'][index])
+    text_tokens_normal = hzm.word_tokenize(normalized)
+    tokens_normal_without_sw = removeStopWords(text_tokens_normal)
+    data['stop_word'][index] = ' '.join(tokens_normal_without_sw)
+
+    lemmatized_tokens = lemma(tokens_normal_without_sw)
+    data['lemmatizer'][index] = ' '.join(lemmatized_tokens)
+
+  data.to_excel("data_norm_stopWord_lemm.xlsx")
+
+
