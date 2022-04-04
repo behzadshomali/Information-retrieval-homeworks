d5edfcb0d661c7df25ec95babe5305d5d3f8fd9f
hojjatre <hojjat7878.h@gmail.com>
2022-04-04 13:30:37 +0430

    tf-id-query

--- homework-2/main.py
+++ homework-2/main.py
@@ -1,8 +1,12 @@
 import pandas as pd
 from utils import *
+from scipy import spatial
 
 
 if __name__ == '__main__':
+
+    query = getPreProccessedInput("تاریخ تحولات سیاسی اجتماعی ایران")
+
     inverted_indexing_df_path = '../data/inverted_indexing.xlsx'
     inverted_indexing_df = pd.read_excel(inverted_indexing_df_path)
 
@@ -11,5 +15,44 @@ if __name__ == '__main__':
     docs_count = preprocessed_df.shape[0]
     print('Total number of docs:', docs_count)
 
-    # idf = get_idf(inverted_indexing_df, docs_count, show_logs=True)
+    idf_query = get_idf_query(query, inverted_indexing_df, docs_count, show_logs=True)
+    print(idf_query)
+    tf_query, tf_sum = get_tf_query(query ,inverted_indexing_df, preprocessed_df, show_logs=True)
+    print(tf_sum)
+
+    sum = 0
+    # vector_query = {}
+    vector_query = []
+    for i in " ".join(query).split(" "):
+        temp = idf_query[f"{i}"] * tf_sum[f"{i}"]
+        sum = sum + temp
+        # vector_query[i] = temp
+        vector_query.append(temp)
+    print(vector_query)
+
+    idf = get_idf(inverted_indexing_df, docs_count, show_logs=True)
     tf = get_tf(inverted_indexing_df, preprocessed_df, show_logs=True)
+
+    # doc_tf_idf = {}
+    # for d_id in tf:
+    #     temp = 0
+    #     for word in tf[d_id]:
+    #         temp = temp + (tf[d_id][word] * idf[word])
+    #     doc_tf_idf[d_id] = temp
+
+    vector_doc_tf_idf = {}
+    for d_id in tf:
+        vector_doc_tf_idf.setdefault(d_id, {})
+        for word in tf[d_id]:
+            vector_doc_tf_idf[d_id][word] = (tf[d_id][word] * idf[word])
+
+
+    # شباهت کوسینوسی- طول بردار پرس و جو و سند یکی نبود!!!
+    for i in vector_doc_tf_idf:
+        temp = []
+        for vec_doc in vector_doc_tf_idf[i]:
+            temp.append(vector_doc_tf_idf[i][vec_doc])
+        print(temp)
+        print(vector_query)
+        cos_dis = spatial.distance.cosine(vector_query, temp)
+        print(cos_dis)
--- homework-2/utils.py
+++ homework-2/utils.py
@@ -1,5 +1,62 @@
 import ast
 import math
+import preprocessing
+import hazm as hzm
+
+def getPreProccessedInput(query):
+    text = preprocessing.normalizer(query)
+    text_tokens = hzm.word_tokenize(text)
+    text_tokens = preprocessing.remove_punctuations(text_tokens)
+    text_tokens = preprocessing.removeStopWords(text_tokens)
+    text_tokens = preprocessing.lemma(text_tokens)
+
+    return text_tokens
+
+def get_idf_query(query,inverted_indexing_df, docs_count, show_logs=False):
+    '''term --> df(doc frequency) --> idf'''
+    idf = {}
+    for term in query:
+        try:
+            term_docs_ids = inverted_indexing_df.loc[inverted_indexing_df.term == term, 'docs_id'].to_list()[0]
+            term_docs_ids = ast.literal_eval(term_docs_ids)
+            doc_frequency = len(term_docs_ids)
+            idf[term] = math.log((docs_count / doc_frequency), 10)
+        except Exception as e:
+            print(e)
+
+        # if show_logs:
+        #     if i % int(inverted_indexing_df.shape[0]/20) == 0:
+        #         print('Computed idf for {}/{} terms!'.format(i, len(inverted_indexing_df)))
+
+    return idf
+
+def get_tf_query(query, inverted_indexing_df, preprocessed_df, show_logs=False):
+    '''term --> doc --> tf'''
+    tf = {}
+    tf_sum = {}
+    for term in query:
+        sum = 0
+        tf.setdefault(term, {})
+        try:
+            term_docs_ids = inverted_indexing_df.loc[inverted_indexing_df.term == term, 'docs_id'].to_list()[0]
+            term_docs_ids = ast.literal_eval(term_docs_ids)
+            for doc_id in term_docs_ids:
+
+                doc_preprocessed_content = preprocessed_df.loc[preprocessed_df.index == doc_id, 'lemmatizer'].to_list()[0]
+                doc_preprocessed_terms = doc_preprocessed_content.split('/')
+                cal = 1 + math.log((doc_preprocessed_terms.count(term)), 10)
+                tf[term][doc_id] = cal
+                sum = sum + cal
+                tf_sum[term] = sum
+        except Exception as e:
+            print(e)
+
+        # if show_logs:
+        #     if i % int(inverted_indexing_df.shape[0]/20) == 0:
+        #         print('Computed tf for {}/{} terms!'.format(i, len(inverted_indexing_df)))
+
+    return tf, tf_sum
+
 
 def get_idf(inverted_indexing_df, docs_count, show_logs=False):
     '''term --> df(doc frequency) --> idf'''
@@ -9,34 +66,38 @@ def get_idf(inverted_indexing_df, docs_count, show_logs=False):
             term_docs_ids = inverted_indexing_df.loc[inverted_indexing_df.term == term, 'docs_id'].to_list()[0]
             term_docs_ids = ast.literal_eval(term_docs_ids)
             doc_frequency = len(term_docs_ids)
-            idf[term] = math.log(docs_count / doc_frequency)
+            idf[term] = math.log((docs_count / doc_frequency), 10)
         except Exception as e:
             print(e)
 
         if show_logs:
-            if i % int(inverted_indexing_df.shape[0]/20) == 0:
+            if i % int(inverted_indexing_df.shape[0] / 20) == 0:
                 print('Computed idf for {}/{} terms!'.format(i, len(inverted_indexing_df)))
 
     return idf
+
 
 def get_tf(inverted_indexing_df, preprocessed_df, show_logs=False):
     '''term --> doc --> tf'''
     tf = {}
     for i, term in enumerate(inverted_indexing_df.term):
-        tf.setdefault(term, {})
         try:
             term_docs_ids = inverted_indexing_df.loc[inverted_indexing_df.term == term, 'docs_id'].to_list()[0]
             term_docs_ids = ast.literal_eval(term_docs_ids)
             for doc_id in term_docs_ids:
-                doc_preprocessed_content = preprocessed_df.loc[preprocessed_df.index == doc_id, 'lemmatizer'].to_list()[0]
+                tf.setdefault(doc_id, {})
+                doc_preprocessed_content = preprocessed_df.loc[preprocessed_df.index == doc_id, 'lemmatizer'].to_list()[
+                    0]
                 doc_preprocessed_terms = doc_preprocessed_content.split('/')
-                
-                tf[term][doc_id] = 1 + math.log(doc_preprocessed_terms.count(term))    
+
+                tf[doc_id][term] = 1 + math.log((doc_preprocessed_terms.count(term)), 10)
         except Exception as e:
             print(e)
 
+
         if show_logs:
-            if i % int(inverted_indexing_df.shape[0]/20) == 0:
+            if i % int(inverted_indexing_df.shape[0] / 20) == 0:
                 print('Computed tf for {}/{} terms!'.format(i, len(inverted_indexing_df)))
-        
+
+
     return tf
