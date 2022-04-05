95890eada1d54e2084f59b74960bdaa1a3177b65
hojjatre <hojjat7878.h@gmail.com>
2022-04-06 02:11:33 +0430

    add cosine-similarity

--- homework-2/main.py
+++ homework-2/main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 from utils import *
 from scipy import spatial
 
@@ -14,29 +15,30 @@ if __name__ == '__main__':
     preprocessed_df = pd.read_excel(preprocessed_df_path)
     docs_count = preprocessed_df.shape[0]
     print('Total number of docs:', docs_count)
-    # print(inverted_indexing_df['term'])
 
-    # idf_query = get_idf_query(query, inverted_indexing_df, docs_count, show_logs=True)
-    # print(idf_query)
-    # tf_query, tf_sum = get_tf_query(query ,inverted_indexing_df, preprocessed_df, show_logs=True)
-    # print(tf_sum)
+    idf_query = get_idf_query(query, inverted_indexing_df, docs_count, show_logs=True)
+    tf_query, tf_sum = get_tf_query(query ,inverted_indexing_df, preprocessed_df, show_logs=True)
 
-    # sum = 0
-    # # vector_query = {}
-    # vector_query = []
-    # for i in " ".join(query).split(" "):
-    #     temp = idf_query[f"{i}"] * tf_sum[f"{i}"]
-    #     sum = sum + temp
-    #     # vector_query[i] = temp
-    #     vector_query.append(temp)
-    # print(vector_query)
 
-    # query_idf = get_idf_query(query, inverted_indexing_df, docs_count, show_logs=True)
-    # query_tf, tf_sum = get_tf_query(query ,inverted_indexing_df, preprocessed_df, show_logs=True)
+    # --- TF*IDF ---------
+    # IN HERE
+    # for QUERY and DOCs
+    sum = 0
+    vector_query = []
 
-    # docs_idf = get_idf(inverted_indexing_df, docs_count, show_logs=True)
-    # docs_tf = get_tf(inverted_indexing_df, preprocessed_df, show_logs=True)
+    for i in " ".join(query).split(" "):
+        temp = idf_query[f"{i}"] * tf_sum[f"{i}"]
+        sum = sum + temp
+        vector_query.append(temp)
 
-    docs_vector = compute_docs_tf_idf(docs_count, preprocessed_df, inverted_indexing_df, show_logs=True)
-    query_vector = compute_query_tf_idf(query, preprocessed_df, inverted_indexing_df)
-    print(ranking(query_vector, docs_vector, 10))
+    idf = get_idf(inverted_indexing_df, docs_count, show_logs=True)
+    tf = get_tf(inverted_indexing_df, preprocessed_df, show_logs=True)
+
+    vector_doc_tf_idf = {}
+    for d_id in tf:
+        vector_doc_tf_idf.setdefault(d_id, {})
+        for word in tf[d_id]:
+            vector_doc_tf_idf[d_id][word] = (tf[d_id][word] * idf[word])
+
+    print(ranking(vector_query, vector_doc_tf_idf, 10))
+
