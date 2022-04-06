02b05474df479b892d8177152352068fe2cebf56
Hojjat <hojjat.rezaei@almasdata.com>
2022-04-06 18:09:05 +0430

    save to json

--- homework-2/main.py
+++ homework-2/main.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 from utils import *
 from threading import Thread
+import json
 
 
 if __name__ == '__main__':
@@ -30,10 +31,15 @@ if __name__ == '__main__':
     for d, d_id in enumerate(preprocessed_df.index):
         for w, word in enumerate(inverted_indexing_df.term):
             vector_doc_tf_idf[d, w] = (tf.get(d_id, {}).get(word, 0) * idf.get(word, 0))
-            
+    
+
+    
     # --------- TF-IDF for QUERY ---------
+    flag = 1
     while True:
-        query = getPreProccessedInput(input('Enter your query: '))
+        data = {}
+        q_input = input('Enter your query: ')
+        query = getPreProccessedInput(q_input)
         tf_query, tf_sum = get_tf_query(query ,inverted_indexing_df, preprocessed_df, show_logs=True)
 
         vector_query = np.zeros(len(inverted_indexing_df.term))
@@ -44,4 +50,14 @@ if __name__ == '__main__':
         matches_indices, matches_distances =  ranking(vector_query, vector_doc_tf_idf, 10)
         print('10 most relevant docs for the input query are as follows:', )
         for i, match_index in enumerate(matches_indices):
+            data[f"num{i}"] = {}
+            data[f"num{i}"]["Query"] = str(q_input)
+            data[f"num{i}"]["Doc_ID"] = str(match_index)
+            data[f"num{i}"]["distance"] = str(matches_distances[i])
+            data[f"num{i}"]["title"] = str(preprocessed_df.iloc[match_index]['title'])
             print(f"Doc_ID: {match_index:5d}, distance: {matches_distances[i]:.5f}/ {preprocessed_df.iloc[match_index]['title']}")
+        
+        with open(f"query_json/Query-{flag}.json", 'w') as outfile:
+            json.dump(data, outfile, ensure_ascii=False)
+        flag = flag + 1
+        data.clear()
